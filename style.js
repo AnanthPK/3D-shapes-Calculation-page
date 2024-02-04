fbdb34c96feadb7b83bbeb8da5ef0d41886c6c64
Ananth <abananth8@gmail.com>
2024-02-04 23:23:09 +0530

    main

---
+++ style.js
@@ -0,0 +1,10 @@
+// function myFun(vol){
+//     var a = document.getElementById("vol").value;
+//     var c = a**3;
+//     document.getElementById("root").innerHTML=c;
+// }
+// function myFuncsa(){
+//     var a = document.getElementById("vol").value;
+//     var b = 4*(a*a);
+//     document.getElementById("root").innerHTML=b;
+// }
